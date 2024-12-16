6022e931a622de61747a524490bdc5de7f994ae3
gstohl <dominik@gstohl.com>
2024-12-16 16:52:59 +0100

    fixed theme provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,10 +1,32 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider, Attribute } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: Attribute
+  defaultTheme?: string
+  enableSystem?: boolean
+}
+
+export function ThemeProvider({ 
+  children,
+  attribute = "class",
+  defaultTheme = "system",
+  enableSystem = true,
+  ...props 
+}: ThemeProviderProps) {
+  return (
+    <NextThemesProvider 
+      attribute={attribute}
+      defaultTheme={defaultTheme}
+      enableSystem={enableSystem}
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
 
