9c4c18b59bffc06d71f46796d0a306d39bc769af
gstohl <dominik@gstohl.com>
2024-12-16 17:21:32 +0100

    added logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
 import "@/styles/globals.css"
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from "@/components/theme-provider"
+import { Metadata } from 'next'
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 })
+
+export const metadata: Metadata = {
+  icons: {
+    icon: '/logo.svg',
+    shortcut: '/logo.svg',
+    apple: '/logo.svg',
+  },
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
